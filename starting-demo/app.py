6e7705cc52ee6c26b150c86dd5de93088cfdb4e3
Johnny Huinac <jhuinac7@gmail.com>
2021-03-24 17:23:20 -0700

    separated the data to a different directory and file

--- starting-demo/app.py
+++ starting-demo/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, redirect, url_for, request, render_template
-
+from data.books import books as books_data
 app = Flask(__name__) #name determines the name of the application, this is the main file of the application
 
 # @app.route('/profile/<int:id>')
@@ -43,25 +43,5 @@ def profile(username):
 
 @app.route('/books')
 def books():
-    books = [
-        {
-            "name": "book1",
-            "author": "author1",
-            "description": "first description",
-            "img": "https://images.unsplash.com/photo-1589998059171-988d887df646?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1055&q=80"
-        },
-        {
-            "name": "book2",
-            "author": "author2",
-            "description": "second description",
-            "img": "https://images.unsplash.com/photo-1589998059171-988d887df646?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1055&q=80"
-        },
-        {
-            "name": "book3",
-            "author": "author3",
-            "description": "third description",
-            "img": "https://images.unsplash.com/photo-1589998059171-988d887df646?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1055&q=80"
-        }
-        ]
-    return render_template('books.html', books=books)
+    return render_template('books.html', books=books_data)
 app.run(debug=True)
---
+++ starting-demo/data/books.py
@@ -0,0 +1,20 @@
+books = [
+        {
+            "name": "book1",
+            "author": "author1",
+            "description": "first description",
+            "img": "https://images.unsplash.com/photo-1589998059171-988d887df646?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1055&q=80"
+        },
+        {
+            "name": "book2",
+            "author": "author2",
+            "description": "second description",
+            "img": "https://images.unsplash.com/photo-1589998059171-988d887df646?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1055&q=80"
+        },
+        {
+            "name": "book3",
+            "author": "author3",
+            "description": "third description",
+            "img": "https://images.unsplash.com/photo-1589998059171-988d887df646?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1055&q=80"
+        }
+        ]
